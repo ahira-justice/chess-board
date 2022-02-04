775b87f9b3935b737c5b5e491d95c4d0f9e5e158
Ahira Adefokun <ahira.adefokun@teamapt.com>
2022-02-04 23:21:50 +0100

    Use starting fen as default for display.start()

--- constants.py
+++ constants.py
@@ -3,6 +3,7 @@ import os
 
 FPS = 30
 IMAGE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')
-
-WINDOW_WIDTH = 600
+STARTING_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+WINDOW_CAPTION = "chess-board"
 WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 600
--- display.py
+++ display.py
@@ -5,8 +5,7 @@ import pygame
 from pygame.locals import QUIT, KEYUP, K_ESCAPE
 
 from board import Board, Color
-from constants import FPS, WINDOW_WIDTH, WINDOW_HEIGHT
-
+from constants import FPS, STARTING_FEN, WINDOW_CAPTION, WINDOW_WIDTH, WINDOW_HEIGHT
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'  # Centre display window.
 
@@ -27,25 +26,16 @@ def check_for_quit():
         pygame.event.post(event)
 
 
-def start(fen='', bg_color=Color.ASH, caption="chess-board"):
+def start(fen=STARTING_FEN, bg_color=Color.ASH, caption=WINDOW_CAPTION):
     pygame.init()
 
     display_surf = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption(caption)
 
-    check_for_quit()
-
     display_surf.fill(bg_color)
     game_board = Board(bg_color, display_surf)
 
-    if not fen:
-        game_board.display_board()
-        game_board.draw_pieces()
-    else:
-        game_board.update_pieces(fen)
-
-    pygame.display.update()
-    fps_clock.tick(FPS)
+    update(fen, game_board)
 
     return game_board
 
